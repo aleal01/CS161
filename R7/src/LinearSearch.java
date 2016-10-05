4267ad0dd6ca9f442e38971c564e347f2dcca812
aleal01 <aaronaleal96@gmail.com>
2016-10-05 11:45:06 -0600

    Updated 1145

--- R7/src/LinearSearch.java
+++ R7/src/LinearSearch.java
@@ -12,17 +12,33 @@ public class LinearSearch implements Searching {
      if (c == n)   Searching element is absent 
         System.out.println(search + " is not present in array.");
     }*/
-    
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
 	public int search(int[] numbersToSearch, int numberToSearchFor) {
+		 int size = numbersToSearch.length;
+	        for(int i=0;i<size;i++){
+	            if(numbersToSearch[i] == numberToSearchFor){
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		return 0;
+		int[] array = {233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368};
+		
+		LinearSearch ls = new LinearSearch();
+//	Search for a number here and print out the index...
+		System.out.println("Linear Search");
+		System.out.println(ls.search(array, 0));
+		System.out.println(ls.search(array, 233));
+		System.out.println(ls.search(array, 2584));
+		System.out.println(ls.search(array, 46368));
+		System.out.println(ls.search(array, 99999));
+		System.out.println();
+
+
 	}
 
 }
