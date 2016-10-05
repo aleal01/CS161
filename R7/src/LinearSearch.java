6a7c547eefbd6c6d817b8fcab599e20ca003852e
aleal01 <aaronaleal96@gmail.com>
2016-10-05 11:01:28 -0600

    Initial Commit

--- R7/src/LinearSearch.java
+++ R7/src/LinearSearch.java
@@ -1,5 +1,18 @@
 
 public class LinearSearch implements Searching {
+	
+	/*for (c = 0; c < n; c++)
+    {
+        if (array[c] == search)      Searching element is present 
+        {
+           System.out.println(search + " is present at location " + (c + 1) + ".");
+            break;
+        }
+     }
+     if (c == n)   Searching element is absent 
+        System.out.println(search + " is not present in array.");
+    }*/
+    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
