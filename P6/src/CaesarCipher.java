08c27ee3e76861309e98946748247bf59dccf004
aleal01 <aaronaleal96@gmail.com>
2016-10-15 09:50:58 -0700

    Initial Commit

--- P6/src/CaesarCipher.java
+++ P6/src/CaesarCipher.java
@@ -1,6 +1,7 @@
 public class CaesarCipher implements Cipher{
 	
 	private int numberOfPositions;
+	private String nstring;
 	
 	public CaesarCipher(int numberOfPositions){
 		this.numberOfPositions = numberOfPositions;
@@ -8,15 +9,31 @@ public class CaesarCipher implements Cipher{
 
 	@Override
 	public String encrypt(String plaintext) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		this.nstring = plaintext;
+		char[] buffer = plaintext.toCharArray();
+
+		// Loop over characters.
+		for (int i = 0; i < buffer.length; i++) {
+
+		    // Shift letter, moving back or forward 26 places if needed.
+		    char letter = buffer[i];
+		    letter = (char) (letter + numberOfPositions);
+		    if (letter > 'z') {
+			letter = (char) (letter - 26);
+		    } else if (letter < 'a') {
+			letter = (char) (letter + 26);
+		    }
+		    buffer[i] = letter;
+		}
+		// Return final string.
+		return new String(buffer);
+	    }
 
 	@Override
 	public String decrypt(String ciphertext) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		// Return final string.
+		return nstring;
+	    }
 	
 	
 	
@@ -24,9 +41,16 @@ public class CaesarCipher implements Cipher{
 	
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+	    CaesarCipher cipher = new CaesarCipher(3);
+	    String plainText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	    System.out.println("Plaintext: " + plainText);
+	    String cipherText = cipher.encrypt(plainText);
+	    System.out.println("After encryption, cipherText: " + cipherText);
+	    String backToPlainText = cipher.decrypt(cipherText);
+	    System.out.println("After decryption, plainText: " + backToPlainText);
+	    if(!plainText.equals(backToPlainText))
+	        System.err.println("Fix your program!");
 	}
 
 }
