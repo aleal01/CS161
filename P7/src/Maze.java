ae4e44f780068df6fcbf53979c7ef114258603ac
aleal01 <aaronaleal96@gmail.com>
2016-10-21 15:02:09 -0600

    Change

--- P7/src/Maze.java
+++ P7/src/Maze.java
@@ -20,25 +20,46 @@ public class Maze implements IMaze {
 
 	@Override
 	public char[][] readFile(String filename) {
-		// TODO Auto-generated method stub
+		/* Precondition - filename set to file containing map of the maze.
+		 * Postcondition - two dimensional char array holding a map of the maze
+		 * Postcondition - returns null if file not found
+		 */
 		return null;
 	}
 
 	@Override
 	public int[] findStart(char[][] maze) {
-		// TODO Auto-generated method stub
+		/* Precondition - maze array initialized to a valid maze
+		 * Postcondition - array containing row, column of location of S 
+		 * 		returned.  Ex. if S is in maze[1][2]. the return 
+		 * 		array is {1, 2}
+		 * Postcondition - returns null if no S found
+		 */
 		return null;
 	}
 
 	@Override
 	public String findPath(char[][] maze, int[] startPosition) {
-		// TODO Auto-generated method stub
+		/* Precondition - maze array initialize to a valid maze
+		 * Precondition - StartPosition contains row, column of location of S 
+		 * 		Ex. if S is in maze[1][2], the startPosition 
+		 * 		 array is {1, 2}
+		 * Postcondition - returns a String composed of the appropriate
+		 *     characters from  'U', 'R', 'D', 'L', and the final 'G', 
+		 *     indicating the solution path.
+		 * Postcondition - successful path marked with '.' characters in maze
+		 * 		 array from 'S' to the final 'G', indicating the 
+		 * 		 solution path.
+		 */
 		return null;
 	}
 
 	@Override
 	public String printMaze(char[][] maze) {
-		// TODO Auto-generated method stub
+		/*
+		 * Postcondition - returns a String representation of the map character array
+		 * 	(complete with end of line characters).
+		 */
 		return null;
 	}
 	
