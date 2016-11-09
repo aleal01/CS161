87a3e842ad28d77b602ab43ec51f07a8a785647e
aleal01 <aaronaleal96@gmail.com>
2016-11-09 11:09:13 -0700

    Initial Commit

---
+++ R12/src/LinkedBookList.java
@@ -0,0 +1,54 @@
+
+
+public class LinkedBookList {
+
+	private BookNode head;
+	private int size;
+	
+	public LinkedBookList(){
+		head = null;
+		size = 0;
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	//IMPLEMENT -- adds a book to the end of the linked list
+	public void add(Book b){
+		return;
+	}
+	
+	//IMPLEMENT -- adds a book at the specific index, or at the end if index is greater than size
+	public void add(Book b, int index){
+		return;
+	}
+	
+	//IMPLEMENT -- removes a book and returns it, or returns null if it is not present
+	public Book remove(Book b){
+		return null;
+	}
+
+	//IMPLEMENT -- removes a book at a specific indexand returns it, 
+	//or returns null if it is not present
+	public Book remove(int index){
+		
+		return null;
+	}
+	
+	//IMPLEMENT -- returns the total number of pages in the linked list
+	public int totalPages(){
+		return 0;
+	}
+	
+	public String toString()
+	{
+		String res = "";
+
+		return res;
+	}
+	
+}
+
+
+
