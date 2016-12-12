e6b30e1fffcdfd6d607771d91ac08a71fe14cf7b
aleal01 <aaronaleal96@gmail.com>
2016-12-12 07:58:41 -0700

    Initial Commit

---
+++ Practive Code!/src/CS163.java
@@ -0,0 +1,12 @@
+public class CS163 {
+
+	double d0 = 2.3456,d1=1.2345;
+	boolean b = (d0 <= d1);
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Help");
+		System.out.print("Hello");
+
+	}
+
+}
